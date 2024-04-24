c5a90ffa88a635b0782b7546ff4a75c92376147a
dnlslcd <danielsalcedoq@gmail.com>
2024-04-24 20:55:55 +0200

    ex4 acabado

--- ex4.js
+++ ex4.js
@@ -11,7 +11,7 @@ let estudiante = {
   nombre: "Edu",
   conocimientos: [],
   aprender: function (nuevoConocimiento) {
-    // una línia de código
+    this.conocimientos.push(nuevoConocimiento);
   },
 };
 
